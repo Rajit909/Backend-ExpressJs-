2ff6d2d92ef15d80ce521e3aa67e7d5069dcff6a
Rajit kumar <krajit909@gmail.com>
2024-02-17 22:20:11 +0530

    insta data fatching

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from "express";
+const express = require("express")
 
 let app = express()
 
@@ -6,20 +6,39 @@ let PORT = 8000;
 
 app.set("view engine", "ejs")
 
-app.get('/', (req, res) => {
-    res.send("<h1>Hello world</h1>")
+
+app.get("/", (req, res)=>{
+    res.render("home.ejs")
 })
-app.get('/search', (req, res) => {
-    let {q }= req.query
-    if(!q){
-        res.send("Nothing searched!")
+app.get("/ig/:username", (req, res)=>{
+    let { username } = req.params;
+    const instaData = require("./data.json")
+    const data = instaData[username]
+    if(data){
+        res.render("insta.ejs", {data})
+    }else{
+        res.render("error.ejs")
     }
-    res.send(`Search query is : ${q}`)
 })
-app.get('/:id/:user', (req, res) => {
-    let {id, user} = req.params;
-    res.send(` <h4> Welcome ${user} ,</h4> <h4> your id is : ${id} </h4>`)
-})
+
+
+
+
+
+// app.get('/', (req, res) => {
+//     res.send("<h1>Hello world</h1>")
+// })
+// app.get('/search', (req, res) => {
+//     let {q }= req.query
+//     if(!q){
+//         res.send("Nothing searched!")
+//     }
+//     res.send(`Search query is : ${q}`)
+// })
+// app.get('/:id/:user', (req, res) => {
+//     let {id, user} = req.params;
+//     res.send(` <h4> Welcome ${user} ,</h4> <h4> your id is : ${id} </h4>`)
+// })
 
 app.listen(PORT, ()=>{
     console.log(`App is running on PORT ${PORT} `);
