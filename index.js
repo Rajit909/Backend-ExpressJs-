49cbb30ef1df7d0e7b6bc370198d33d6de1fb524
Rajit kumar <krajit909@gmail.com>
2024-02-21 22:54:32 +0530

    updating post

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import path from "path"
 
 const __dirname = path.resolve();
 
+import browserSync from "browser-sync";
+
 const app = express();
 
 const PORT = 8000;
@@ -18,20 +20,33 @@ app.listen(PORT, () => {
     console.log(`App is running on http://localhost:${PORT}`);
 })
 
+// Start Browsersync
+const bs = browserSync.create();
+bs.init({
+    proxy: 'http://localhost:8000', // Your app's URL
+    files: ['views/**/*.ejs', 'public/**/*.*'], // Files to watch
+    port: 3000, // Browsersync port
+    notify: false, // Hide the notification
+    reloadDelay: 1000 // Delay to reload the browser
+});
+
 app.get("/", (req, res) => {
     res.send("Surver is running, Welcome")
 })
 
 let posts = [
     {
+        id: "1a",
         username: "rajit",
         content: "I love coding"
     },
     {
+        id: "2a",
         username: "sonu",
         content: "I love reading"
     },
     {
+        id: "2b",
         username: "vikash",
         content: "I love cooking"
     },
@@ -50,4 +65,12 @@ app.post("/posts", (req, res) => {
     posts.push({username, content})
     res.redirect("/posts")
     res.send("Post request")
+})
+
+app.get("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    console.log(id);
+    let post = posts.find((p) => id === p.id);
+    console.log(post);
+    res.send("req is working")
 })
