bb0dc46d327ac739f2a1f9cd12330a8941c29dc3
Rajit kumar <krajit909@gmail.com>
2024-02-21 21:59:35 +0530

    creating new post

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import express from "express";
 import path from "path"
 
+const __dirname = path.resolve();
+
 const app = express();
 
 const PORT = 8000;
 
 app.set("view engine", "ejs")
-// app.set("views", path.join(__dirname, "/views"))
-// app.use(express.static(path.join(__dirname, "public")))
+app.set("views", path.join(__dirname, "/views"))
+app.use(express.static(path.join(__dirname, "public")))
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
@@ -17,4 +19,27 @@ app.listen(PORT, () => {
 
 app.get("/", (req, res) => {
     res.send("Surver is running, Welcome")
+})
+
+let posts = [
+    {
+        username: "rajit",
+        content: "I love coding"
+    },
+    {
+        username: "sonu",
+        content: "I love reading"
+    },
+    {
+        username: "vikash",
+        content: "I love cooking"
+    },
+]
+
+app.get("/posts", (req, res) => {
+    res.render("index.ejs", {posts})
+})
+
+app.post("/posts/new", (req, res) => {
+    res.render("new.ejs")
 })
