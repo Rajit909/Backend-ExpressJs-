44e310c0e5958ad0e8f59eeb3ef0b8cc874cbac7
Rajit kumar <krajit909@gmail.com>
2024-02-24 21:20:59 +0530

    deleting post

--- index.js
+++ index.js
@@ -88,3 +88,8 @@ app.get("/posts/:id/edit", (req, res) => {
 
     res.render("edit.ejs", {post})
 })
+app.delete("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect("/posts")
+})
